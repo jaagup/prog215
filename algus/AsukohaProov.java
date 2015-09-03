928a842a92e948fb1f86f0c2eda77e93fede9db2
jaagup@tlu.ee <jaagup@tlu.ee>
2015-09-03 12:01:29 +0300

    konstruktor

--- algus/AsukohaProov.java
+++ algus/AsukohaProov.java
@@ -1,12 +1,8 @@
 public class AsukohaProov{
    public static void main(String[] arg){
-      Asukoht haapsalu=new Asukoht();
-	  haapsalu.lat=58.936864;
-	  haapsalu.lng=23.540753;
-	  Asukoht tartu=new Asukoht();
-	  tartu.lat=58.371829;
-	  tartu.lng=26.729506;
-	  System.out.println("Ajavahe: "+
-	    (tartu.lng-haapsalu.lng)/360*24*60+" minutit");
+      Asukoht haapsalu=new Asukoht(58.936864, 23.540753);
+	  Asukoht tartu=new Asukoht(58.371829, 26.729506);
+      System.out.println(haapsalu+" "+tartu);
+	  System.out.println(haapsalu.lat);
    }
 }
--- algus/Asukoht.java
+++ algus/Asukoht.java
@@ -1,4 +1,14 @@
 class Asukoht{
+   public Asukoht(double laiuskraad, double pikkuskraad){
+      lat=kraadParajaks(laiuskraad);
+	  lng=kraadParajaks(pikkuskraad);
+   }
+   private double kraadParajaks(double kraad){
+      while(kraad>180) {kraad-=360;}
+      while(kraad<-180){kraad+=360;}
+	  return kraad;
+   }
+   public @Override String toString(){ return "("+lat+","+lng+")";}
    double lat;
    double lng;
 }
