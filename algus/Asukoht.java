8f934d1808bcfc0bbff099c2b6c59f3b78ff1e46
jaagup@tlu.ee <jaagup@tlu.ee>
2015-09-03 12:12:51 +0300

    andmehoiu muutus

--- algus/Asukoht.java
+++ algus/Asukoht.java
@@ -1,16 +1,16 @@
 class Asukoht{
    public Asukoht(double laiuskraad, double pikkuskraad){
-      lat=kraadParajaks(laiuskraad);
-	  lng=kraadParajaks(pikkuskraad);
+      koordinaadid[0]=kraadParajaks(laiuskraad);
+	  koordinaadid[1]=kraadParajaks(pikkuskraad);
    }
    private double kraadParajaks(double kraad){
       while(kraad>180) {kraad-=360;}
       while(kraad<-180){kraad+=360;}
 	  return kraad;
    }
-   public @Override String toString(){ return "("+lat+","+lng+")";}
-   private double lat;
-   private double lng;
-   public double getLat(){return lat;}
-   public double getLng(){return lng;}
+   public @Override String toString(){ 
+      return "("+this.getLat()+","+this.getLng()+")";}
+   private double[] koordinaadid=new double[2];
+   public double getLat(){return koordinaadid[0];}
+   public double getLng(){return koordinaadid[1];}
 }
